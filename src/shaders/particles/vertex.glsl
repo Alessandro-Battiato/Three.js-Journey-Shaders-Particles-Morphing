b8bc3786987cefdb16babf044efecd989a676afb
alessandrobattiato <alessandroDev@outlook.it>
2024-05-26 18:24:04 +0200

    fix: simplex noise morphing effect now works for all the models whom are used as position reference

--- src/shaders/particles/vertex.glsl
+++ src/shaders/particles/vertex.glsl
@@ -11,7 +11,9 @@ varying vec3 vColor;
 void main()
 {
     // Mixed position
-    float noise = simplexNoise3d(position); // remember that position is a vec3
+    float noiseOrigin = simplexNoise3d(position); // remember that position is a vec3
+    float noiseTarget = simplexNoise3d(aPositionTarget);
+    float noise = mix(noiseOrigin, noiseTarget, uProgress); // this fixed the issue where we were using Suzanne as initial refernce for position, which made simplex noise work only at the beginning, with Suzanne morphing into the Three.js text, but not the other way around, and this fixes the issue
     noise = smoothstep(-1.0, 1.0, noise);
 
     float duration = 0.4;
